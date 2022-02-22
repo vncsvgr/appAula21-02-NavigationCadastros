058bbced76d01fa5844ff6749f3c39dd4819a9a2
Usuario <Usuario@CPD03-M12>
2022-02-21 22:18:02 -0300

    Adicionar arquivos de projeto.

---
+++ appNavigationAula21_02/appNavigationAula21_02/Cursos.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appNavigationAula21_02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cursos : ContentPage
+    {
+        public Cursos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGravar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        async void btnVoltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ appNavigationAula21_02/appNavigationAula21_02/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appNavigationAula21_02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlunos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProfessores_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        async void btnCursos_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cursos());
+        }
+
+        private void btnDisciplinas_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
